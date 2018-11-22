62451a88e62d36e09a19910de37048815fe27cde
RicardCabezas <r.cabezas.diez@gmail.com>
2018-11-22 22:58:08 +0100

    Puzzle(starting)

--- MusicPrototype_Riffenberg'sMusicBox_1/Assets/Scripts/pieceSript.cs
+++ MusicPrototype_Riffenberg'sMusicBox_1/Assets/Scripts/pieceSript.cs
@@ -6,21 +6,45 @@ public class pieceSript : MonoBehaviour
 {
 
     public GridScript theGrid;
-    Node myNode = null;
+    [SerializeField] private Transform[] m_Positions;
+    private Node[] m_Nodes;
+    private int m_CurrentPositionIndex = 0;
 
     // Use this for initializations
     void Start()
     {
+        m_Nodes = new Node[m_Positions.Length];
+        for (int i = 0; i < m_Positions.Length; i++)
+        {
+            m_Nodes[i] = theGrid.GetNodeContainingPosition(m_Positions[i].position);
+            //Destroy(m_Positions[i].gameObject);
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.anyKeyDown)
-        //{
-        //	myNode = theGrid.getNodeContainingPosition(transform.position);
-        //}
 
+    }
+
+    public void MoveWallToNextPosition()
+    {
+        if(m_CurrentPositionIndex < m_Nodes.Length - 1)
+        {
+            m_CurrentPositionIndex++;
+        }
+        transform.position = m_Nodes[m_CurrentPositionIndex].worldPosition;
+        theGrid.GenerateGrid();
+
+    }
+    public void MoveWallToPreviousPosition()
+    {
+        if (m_CurrentPositionIndex > 0)
+        {
+            m_CurrentPositionIndex--;
+        }
+        transform.position = m_Nodes[m_CurrentPositionIndex].worldPosition;
+        theGrid.GenerateGrid();
     }
 }
