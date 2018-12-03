8ff74086123deced3a558edd7bc4555ee41f4c23
adrinuga <38584756+adrinuga@users.noreply.github.com>
2018-12-04 00:08:07 +0100

    cajitacajita

---
+++ MusicPrototype_Riffenberg'sMusicBox_1/Assets/Scripts/InteractableObjects/FinalButtonInt.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FinalButtonInt : InteractableObject
+{
+
+    [SerializeField] private UnityEvent m_event;
+
+    [SerializeField] private Outline m_objectOutline;
+
+
+
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        GameManager.m_instance.m_interactableObjects.Add(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_objectOutline.enabled = false;
+    }
+    public override void OnClick()
+    {
+        if (GameManager.m_instance.m_playerNav.m_BoxOn)
+        {
+
+            m_event.Invoke();
+
+            m_objectOutline.enabled = false;
+
+
+
+        }
+    }
+    public override void MouseOver()
+    {
+       
+        if (GameManager.m_instance.m_playerNav.m_BoxOn)
+        {
+            Debug.Log("finalb");
+            m_objectOutline.enabled = true;
+        }
+
+    }
+    public override Transform ReturnObject()
+    {
+        return this.transform;
+    }
+}
