dc4a63f5ebb769e1f2e52abef558a3ab5eefbb9d
RicardCabezas <r.cabezas.diez@gmail.com>
2018-12-11 01:05:25 +0100

    Fan

---
+++ MusicPrototype_Riffenberg'sMusicBox_1/Assets/Scripts/Fan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan: MonoBehaviour
+{
+    [SerializeField] private float m_Speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, m_Speed * Time.deltaTime, 0);
+    }
+}
+
